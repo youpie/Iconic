5e576064212b3b20498d36a9f3b58b77ba2484fe
youpie <youplamb@hotmail.nl>
2024-04-24 16:32:20 +0200

    Wow a file picker

--- src/window.rs
+++ src/window.rs
@@ -18,7 +18,10 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+use crate::glib::clone;
 use adw::subclass::prelude::*;
+use ashpd::desktop::file_chooser::{Choice,  SelectedFiles};
+use ashpd::WindowIdentifier;
 use gtk::prelude::*;
 use gtk::{gio, glib};
 
@@ -39,6 +42,8 @@ mod imp {
         pub button1: TemplateChild<gtk::Button>,
         #[template_child]
         pub toast_overlay: TemplateChild<adw::ToastOverlay>,
+        #[template_child]
+        pub button2: TemplateChild<gtk::Button>,
     }
 
     #[glib::object_subclass]
@@ -52,6 +57,26 @@ mod imp {
             Self::Type::bind_template_callbacks(klass);
             klass.install_action("app.button_clicked", None, move |win, _, _| {
                 win.button_clicked();
+            });
+            klass.install_action("app.file_picker", None, move |win, _, _| {
+                glib::spawn_future_local(clone!(@weak win => async move {
+                    // Get native of button for window identifier
+                //     let native = win.native().expect("Need to be able to get native.");
+                    // Get window identifier so that the dialog will be modal to the main window
+                //     let identifier = WindowIdentifier::from_native(&native).await;
+                //     let request = UserInformation::request()
+                //         .reason("App would like to access user information.")
+                //         .identifier(identifier)
+                //         .send()
+                //         .await;
+
+                //     if let Ok(response) = request.and_then(|r| r.response()) {
+                //         println!("User name: {}", response.name());
+                //     } else {
+                //         println!("Could not access user information.")
+                //     }
+                    win.open_file_chooser().await;
+                }));
             });
         }
 
@@ -86,6 +111,34 @@ impl GtkTestWindow {
         println!("Button Pressed");
         self.imp().toast_overlay.add_toast(adw::Toast::new("TOATS"));
         self.imp().button1.set_label("JE MOEDER");
+    }
+
+    #[template_callback]
+    pub async fn open_file_chooser(&self) {
+        let native = self
+            .imp()
+            .button2
+            .native()
+            .expect("Need to be able to get native.");
+        // Get window identifier so that the dialog will be modal to the main window
+        let identifier = WindowIdentifier::from_native(&native).await;
+        let files = SelectedFiles::open_file()
+            .title("open a file to read")
+            .accept_label("open")
+            .modal(true)
+            .multiple(true)
+            .identifier(identifier)
+            .choice(
+                Choice::new("encoding", "Encoding", "latin15")
+                    .insert("utf8", "Unicode (UTF-8)")
+                    .insert("latin15", "Western"),
+            )
+            // A trick to have a checkbox
+            .choice(Choice::boolean("re-encode", "Re-encode", false))
+            .send()
+            .await;
+
+        println!("{:#?}", files);
     }
 
     #[template_callback]
