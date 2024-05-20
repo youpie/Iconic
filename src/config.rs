c72663237897266a95787889a95901daa9db1c39
Youp Lamb <youpie@hendrick-jan.taile2c88.ts.net>
2024-05-20 19:30:56 +0200

    Fixed program not compiling

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "folder_icon";
 pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/gtk_test";
+pub static PKGDATADIR: &str = "/app/share/folder_icon";
