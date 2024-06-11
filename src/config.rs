56452c2f9c9fe12b05ffcfe9ab8a24d553f8f556
youpie <youplamb@hotmail.nl>
2024-06-11 18:19:21 +0200

    Added devel and default build option

--- src/config.rs
+++ src/config.rs
@@ -3,4 +3,5 @@ pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "folder_icon";
 pub static LOCALEDIR: &str = "/app/share/locale";
 pub static PKGDATADIR: &str = "/app/share/folder_icon";
+pub static PROFILE: &str = "";
 
