c6d1c908482fbd774f7d56c3d13f0bad0ca17f0b
youpie <youplamb@hotmail.nl>
2025-06-12 11:12:52 +0200

    deleted config.rs

--- src/config.rs
+++
@@ -1,8 +0,0 @@
-pub static APP_ID: &str = "nl.emphisia.icon.Devel";
-pub static VERSION: &str = "2025.3.2-2528778";
-pub static GETTEXT_PACKAGE: &str = "folder_icon";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/folder_icon";
-pub static PROFILE: &str = "Devel";
-pub static APP_ICON: &str = "nl.emphisia.icon.Devel";
-
